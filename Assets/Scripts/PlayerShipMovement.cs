bb215610fdae041f5087ef688b33985518899d04
Sascha Kowark <kowark@inmediasp.de>
2016-01-13 10:55:00 +0100

    Player Movement

--- Assets/Scripts/PlayerShipMovement.cs
+++ Assets/Scripts/PlayerShipMovement.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class ShipMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
+
+    public Vector2 moveDirection;
+
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        float x = Input.GetAxis(HORIZONTAL);
+        float y = Input.GetAxis(VERTICAL);
+
+        moveDirection = new Vector2(x,y);
+    }
+
+    void FixedUpdate() {
+        rb.AddForce(transform.up * moveDirection.x);
+    }
 }
