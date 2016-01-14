7086d34beaca95cbf16eb61ec43dbc7e3b2e3afa
Sascha Kowark <kowark@inmediasp.de>
2016-01-14 13:13:49 +0100

    EngineControl

---
+++ Assets/Scripts/Spaceship/EngineControls.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EngineControls : MonoBehaviour {
+
+    private float power = 0;
+	private float startSpeedMax;
+	private ParticleSystem ps;
+
+	void Start(){
+		ps = GetComponent<ParticleSystem> ();
+		startSpeedMax = ps.startSpeed;
+	}
+
+	// Update is called once per frame
+	void Update () {
+        power = Mathf.Abs(0);
+        power = Mathf.Clamp01(power);
+        float speed = startSpeedMax * power;
+		ps.startSpeed = speed;
+   
+    }
+
+
+}
