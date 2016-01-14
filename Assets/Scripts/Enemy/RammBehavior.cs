54a5146ad2ac5c8170462c93a949be0d57165b9c
Kai <kai.heuermann@inmediasp.de>
2016-01-14 18:03:02 +0100

    enemies

---
+++ Assets/Scripts/Enemy/RammBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RammBehavior : EnemyAttackBehavior {
+
+	public override Vector3 GetNavigationPosition () {
+		return player == null ? destination : destination = player.position;
+	}
+
+	// Use this for initialization
+	new void Start () {
+		base.Start ();
+	}
+	
+	// Update is called once per frame
+	new void Update () {
+		base.Update ();
+	}
+}
