9959460695cc7352db791256832f373a73c2b688
Kai <kai.heuermann@inmediasp.de>
2016-01-14 21:15:17 +0100

    better saw

--- Assets/Scripts/Enemy/RammBehavior.cs
+++ Assets/Scripts/Enemy/RammBehavior.cs
@@ -3,8 +3,18 @@ using System.Collections;
 
 public class RammBehavior : EnemyAttackBehavior {
 
+	private int round = 0;
+
 	public override Vector3 GetNavigationPosition () {
-		return player == null ? destination : destination = player.position;
+		if (player == null) {
+			return destination;
+		}
+
+		if (++round > 20) {
+			transform.RotateAround (Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+			round = 0;
+		}
+		return destination = player.position;
 	}
 
 	// Use this for initialization
