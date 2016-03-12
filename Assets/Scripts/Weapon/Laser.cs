72cccb55fce1057c08935473e01517c3cb7d65f6
Sascha <saschakowark@googlemail.com>
2016-03-12 15:39:58 +0100

    changed weapon script/object structure

--- Assets/Scripts/Weapon/Laser.cs
+++ Assets/Scripts/Weapon/Laser.cs
@@ -1,35 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+namespace Weapon {
 [RequireComponent (typeof (Rigidbody))]
-public class Laser : MonoBehaviour {
-
-	public float bulletSpeed;
-    public int damage;
-
-
-	private Rigidbody rb;
-    
-	// Use this for initialization
-    void Start () {
-		rb = GetComponent<Rigidbody>();
-	}
+public class Laser : AbstractWeapon{
+		
+		public float bulletSpeed;
 	
-	// Update is called once per frame
-	void FixedUpdate () {
-		rb.velocity = transform.forward * bulletSpeed;
-	}
 
-	void OnTriggerEnter(Collider other) {
-        if (!other.tag.Equals(this.tag))
-        { 
-			if (other.gameObject.GetComponent<Hitable>() != null)
-			{
-				Debug.Log("Damage " + other.gameObject);
-				other.gameObject.GetComponent<Hitable>().takeDamage(damage);
-			}
-			Destroy(this.gameObject);
-        }
-       
-    }
+
+		private Rigidbody rb;
+
+		public override void Fire(GameObject origin){
+			
+		}
+
+		// Use this for initialization
+		void Start () {
+			rb = GetComponent<Rigidbody>();
+			Destroy(this.gameObject, lifetime);
+		}
+
+		// Update is called once per frame
+		void FixedUpdate () {
+			rb.velocity = transform.forward * bulletSpeed;
+		}
+
+		void OnTriggerEnter(Collider other) {
+		    if (!other.tag.Equals(this.tag))
+		    { 
+				if (other.gameObject.GetComponent<Hitable>() != null)
+				{
+					Debug.Log("Damage " + other.gameObject);
+					other.gameObject.GetComponent<Hitable>().takeDamage(damage);
+				}
+				Destroy(this.gameObject);
+		    }
+		   
+		}
+	}
 }
--- Assets/Scripts/Weapon/LaserBeam.cs
+++ Assets/Scripts/Weapon/LaserBeam.cs
@@ -2,92 +2,109 @@
 using UnityEngine;
 using System.Collections;
 
+namespace Weapon {
 [RequireComponent (typeof(LineRenderer))]
-public class LaserBeam : MonoBehaviour {
+	public class LaserBeam : AbstractWeapon  {
+		
+		public float laserWidth = 1.0f;
+		public float noise = 1.0f;
+		public float maxLength = 50.0f;
+		public Color color = Color.red;
 
-	public float laserWidth = 1.0f;
-	public float noise = 1.0f;
-	public float maxLength = 50.0f;
-	public Color color = Color.red;
+		LineRenderer lineRenderer;
+		int length;
+		Vector3[] position;
+		//Cache any transforms here
+		Transform myTransform;
+		Transform endEffectTransform;
+		//The particle system, in this case sparks which will be created by the Laser
+		public ParticleSystem endEffect;
+		Vector3 offset;
 
 
-	LineRenderer lineRenderer;
-	int length;
-	Vector3[] position;
-	//Cache any transforms here
-	Transform myTransform;
-	Transform endEffectTransform;
-	//The particle system, in this case sparks which will be created by the Laser
-	public ParticleSystem endEffect;
-	Vector3 offset;
-
-
-	// Use this for initialization
-	void Start () {
-		lineRenderer = GetComponent<LineRenderer>();
-		lineRenderer.SetWidth(laserWidth, laserWidth);
-		myTransform = transform;
-		offset = new Vector3(0,0,0);
-		endEffect = GetComponentInChildren<ParticleSystem>();
-		if (endEffect) {
-			endEffectTransform = endEffect.transform;
+		public override void Fire(GameObject origin){
+			myTransform = origin.transform;
 		}
-	}
 
-	// Update is called once per frame
-	void Update () {
-		RenderLaser();
-	}
-
-	void RenderLaser(){
-		//Shoot our laserbeam forwards!
-		UpdateLength();
-
-		lineRenderer.SetColors(color,color);
-		//Move through the Array
-		for(int i = 0; i<length; i++){
-			//Set the position here to the current location and project it in the forward direction of the object it is attached to
-			offset.x =myTransform.position.x+i*myTransform.forward.x+Random.Range(-noise,noise);
-			offset.z =i*myTransform.forward.z+Random.Range(-noise,noise)+myTransform.position.z;
-			position[i] = offset;
-			position[0] = myTransform.position;
-
-			lineRenderer.SetPosition(i, position[i]);
-
-		}
-	}
-
-	void UpdateLength(){
-		//Raycast from the location of the cube forwards
-		RaycastHit[] hit;
-		hit = Physics.RaycastAll(myTransform.position, myTransform.forward, maxLength);
-		int i = 0;
-		while(i < hit.Length){
-			//Check to make sure we aren't hitting triggers but colliders
-			if(!hit[i].collider.isTrigger)
-			{
-				length = (int)Mathf.Round(hit[i].distance)+2;
-				position = new Vector3[length];
-				//Move our End Effect particle system to the hit point and start playing it
-				if(endEffect){
-					endEffectTransform.position = hit[i].point;
-					if(!endEffect.isPlaying)
-						endEffect.Play();
-				}
-				lineRenderer.SetVertexCount(length);
-				return;
+		// Use this for initialization
+		void Start () {
+			lineRenderer = GetComponent<LineRenderer>();
+			lineRenderer.SetWidth(laserWidth, laserWidth);
+			//myTransform = transform;
+			offset = new Vector3(0,0,0);
+			endEffect = GetComponentInChildren<ParticleSystem>();
+			if (endEffect) {
+				endEffectTransform = endEffect.transform;
 			}
-			i++;
+			Destroy(this.gameObject, lifetime);
 		}
-		//If we're not hitting anything, don't play the particle effects
-		if(endEffect){
-			if(endEffect.isPlaying)
-				endEffect.Stop();
+
+		// Update is called once per frame
+		void Update () {
+			RenderLaser();
 		}
-		length = (int)maxLength;
-		position = new Vector3[length];
-		lineRenderer.SetVertexCount(length);
 
+		void RenderLaser(){
+			//Shoot our laserbeam forwards!
+			UpdateLength();
 
+			lineRenderer.SetColors(color,color);
+			//Move through the Array
+			for(int i = 0; i<length; i++){
+				//Set the position here to the current location and project it in the forward direction of the object it is attached to
+				offset.x =myTransform.position.x+i*myTransform.forward.x+Random.Range(-noise,noise);
+				offset.z =i*myTransform.forward.z+Random.Range(-noise,noise)+myTransform.position.z;
+				position[i] = offset;
+				position[0] = myTransform.position;
+
+				lineRenderer.SetPosition(i, position[i]);
+
+			}
+		}
+
+		void UpdateLength(){
+			//Raycast from the location of the cube forwards
+			RaycastHit[] hit;
+			hit = Physics.RaycastAll(myTransform.position, myTransform.forward, maxLength);
+			int i = 0;
+			while(i < hit.Length){
+				//Check to make sure we aren't hitting triggers but colliders
+				if(!hit[i].collider.isTrigger)
+				{
+					length = (int)Mathf.Round(hit[i].distance)+2;
+					position = new Vector3[length];
+					//Move our End Effect particle system to the hit point and start playing it
+					if(endEffect){
+						endEffectTransform.position = hit[i].point;
+						if(!endEffect.isPlaying)
+							endEffect.Play();
+					}
+					lineRenderer.SetVertexCount(length);
+					OnTriggerEnter (hit [i].collider);
+					return;
+				}
+				i++;
+			}
+			//If we're not hitting anything, don't play the particle effects
+			if(endEffect){
+				if(endEffect.isPlaying)
+					endEffect.Stop();
+			}
+			length = (int)maxLength;
+			position = new Vector3[length];
+			lineRenderer.SetVertexCount(length);
+		}
+
+		void OnTriggerEnter(Collider other) {
+			Debug.Log (this.name + "hit" + other.name);
+			if (!other.tag.Equals(this.tag))
+			{ 
+				if (other.gameObject.GetComponent<Hitable>() != null)
+				{
+					Debug.Log("Damage " + other.gameObject);
+					other.gameObject.GetComponent<Hitable>().takeDamage(damage);
+				}
+			}
+		}
 	}
 }
