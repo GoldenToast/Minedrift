82e01bf133dc243471c0a0bde8e74c3058bfbc2e
Sascha <saschakowark@googlemail.com>
2016-04-19 21:48:33 +0200

    Add spaceman walk around planet

--- Assets/Scripts/Human/PlayerPersonController.cs
+++ Assets/Scripts/Human/PlayerPersonController.cs
@@ -13,32 +13,47 @@ public class PlayerPersonController : MonoBehaviour {
 
 	public float forwardSpeed;
 
+	private bool facingRight;
 	private Vector2 moveDirection;
+	private GameObject centerOfRotation;
 
 	// Use this for initialization
 	void Start () {
+		facingRight = true;
 		rb = GetComponent<Rigidbody>();
 		animator = GetComponent<Animator>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (GetComponent<FauxGravityBody> ().getAttractor()) {
+			createPlayerPointOfRotation ();
+		}
 		if (playerNumber == 1) {
 			handleInput(HORIZONTAL1);
 			handleAnimation ();
 		}
 	}
-
-
-	void moveForward(float amount){
-		rb.AddForce(transform.right * -1 * amount * forwardSpeed,ForceMode.Impulse);
-	}
 		
-	void FixedUpdate() {
-		Debug.Log ("Move Person: " + moveDirection);
-		moveForward (moveDirection.x);
+	private void createPlayerPointOfRotation(){
+		if (!centerOfRotation) {
+			Vector3 attractorCenter = GetComponent<FauxGravityBody> ().getAttractor().transform.position;
+			centerOfRotation = new GameObject();
+			centerOfRotation.name = this.name;
+			centerOfRotation.transform.position = attractorCenter;
+			transform.SetParent (centerOfRotation.transform);
+		}
+			
 	}
 
+	void FixedUpdate() {
+		move (moveDirection.x);
+	}
+
+	private void move(float amount){
+		centerOfRotation.transform.Rotate(transform.up * -1 * amount * forwardSpeed);
+	}
 
 	private void handleInput(String horizontal){
 		float x = Input.GetAxis(horizontal);
@@ -47,10 +62,19 @@ public class PlayerPersonController : MonoBehaviour {
 
 
 	private void handleAnimation(){
-		if (moveDirection.x < 0) {
-			animator.SetBool ("walk_east", true);
+		if (facingRight && moveDirection.x > 0 || !facingRight && moveDirection.x < 0) {
+			facingRight = !facingRight;
+			// Multiply the player's x local scale by -1
+			Vector3 theScale = transform.localScale;
+			theScale.x *= -1;
+			transform.localScale = theScale;
+		}
+		if (moveDirection.x > 0) {
+			animator.SetInteger ("walkLR", 1);
+		} else if (moveDirection.x < 0) {
+			animator.SetInteger ("walkLR", -1);
 		} else {
-			animator.SetBool ("walk_east", false);
+			animator.SetInteger ("walkLR", 0);
 		}
 	}
 }
